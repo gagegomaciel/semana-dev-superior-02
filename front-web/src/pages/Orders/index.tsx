3eaccbadce5450a7cf1e3477f6852db80c0d327c
Gagego Maciel <gagego.maciel@gmail.com>
2021-01-08 09:02:01 -0300

    Dynamically consuming API to list products

--- front-web/src/pages/Orders/index.tsx
+++ front-web/src/pages/Orders/index.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './styles.css';
 
 import StepsHeader from '../../Components/HeaderSteps';
+import Products from '../../Components/ProductsList';
+
+import api from '../../services/api';
+
+import { Product } from '../../@Types/types';
 
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    api.get(`products`).then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <>
       <div className="orders-container">
         <StepsHeader />
+        <Products products={products} />
       </div>
     </>
   )
